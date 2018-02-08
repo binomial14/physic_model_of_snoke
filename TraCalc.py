a1a26603507600ccdf26dc1852bfe6eb7186f93d
wei851010 <wei851010@cavedu.com>
2018-02-08 11:49:03 +0800

    agg global vars

--- TraCalc.py
+++ TraCalc.py
@@ -1,27 +1,42 @@
 import ball
 import json
 
-class traCalc:
+class TraCalc:
+
+    global WIDTH
+    global HEIGHT
+    global RADIUS
+    global CORNER_WIDTH
+    global BALLS_LIST
+    global STOKE
+
+
     def __init__(self):
         ballList = []
         lineList = [[]]
+        nBalls = 0
         
     def run(self):
         read()
-        motion(ballList[0])
+        target = checkTarget()
+        motion(target)
         write()
 
-    def read():
-        data = json.loads(open('data.json').read())
-        Ball.width = data['width'] 
-        Ball.height = data['height'] 
-        Ball.radius = data['radius']
-        balls = data['balls']
+    def read(self):
+        #data = json.loads(open('data.json').read())
+        Ball.width = WIDTH 
+        Ball.height = HEIGHT
+        Ball.radius = RADIUS
+        Ball.corner_width = CORNER_WIDTH
+        nBalls = len(BALLS_LIST)
 
-        for id in range(len(balls)):
-            ball = balls[id]
-            position = Pos(ball[0], ball[1])
-            ballList.append(Ball(id, position))
+        for id in range(nBalls):
+            ballList.append(Ball(id, BALLS_LIST[id]))
+
+    def checkTarget(self):
+        for id in range(nBalls):
+            print('a')
+
 
     def motion(ball):
         id = checkCollision(ball) 
@@ -35,20 +50,16 @@ class traCalc:
             ball.bounce()
 
     def write():
-        lines = dict()
-        for id in range(nBalls):
-            lines.update(id, lineList[id]) 
-        a = json.dumps(lines)
-        print(a)
+        print(lineList)
 
     def checkCollision(ball):
-        nearest = [-1, 10000] 
+        nearest = -1, 10000
         for id in range(nBalls):
             if id == ball.id:
                 continue
             #if more than one ball collides, need to compare distance of projection
             distance = ball.checkCollision(ballList[id])
             if distance != -1 & distance < nearest[1]:
-                nearest = [id, distance]
+                nearest = id, distance
         return nearest[0]
     
