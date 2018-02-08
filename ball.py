015f8041e46b91a422f8433f29e90f531301a971
falconkid <leowinston2017@gmail.com>
2018-02-08 14:41:47 +0800

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,9 +1,14 @@
+import math
+
 class Ball:
-    width = 1440
-    height = 900
-    radius = 15
+    width = 1440.0
+    height = 900.0
+    radius = 15.0
     
     def __init__(self, id, position = (0,0), heading = (0,0)):
+        width = 1440.0
+        height = 900.0
+        radius = 15.0
         self.id = id
         self.position = position
         #determine the end point
@@ -11,51 +16,49 @@ class Ball:
         self.endpoint = self.cal_endpoint()
     
     def cal_endpoint(self):
-        width = 1440.0
-        height = 900.0
-        radius = 15.0
+        
         if self.heading[0] == 0:
             if self.heading[1] == 0:
                 return self.position
             elif self.heading[1] > 0:
-                return (self.position[0],height)
+                return (self.position[0],self.height)
             elif self.heading[1] < 0:
                 return (self.position[0],0.0)
     
         elif self.heading[0] > 0:
             if self.heading[1] == 0:
-                return (width,self.position[1])
+                return (self.width,self.position[1])
             elif self.heading[1] > 0:
-                if (width-self.position[0])/self.heading[0] > (height-self.position[1])/self.heading[1]:
-                    return (self.position[0]+(self.heading[0])*((height-self.position[1])/self.heading[1]),height)
-                elif (width-self.position[0])/self.heading[0] < (height-self.position[1])/self.heading[1]:
-                    return (width,self.position[1]+(self.heading[1])*((width-self.position[0])/self.heading[0]))
+                if (self.width-self.position[0])/self.heading[0] > (self.height-self.position[1])/self.heading[1]:
+                    return (self.position[0]+(self.heading[0])*((self.height-self.position[1])/self.heading[1]),self.height)
+                elif (self.width-self.position[0])/self.heading[0] < (self.height-self.position[1])/self.heading[1]:
+                    return (self.width,self.position[1]+(self.heading[1])*((self.width-self.position[0])/self.heading[0]))
                 else:
-                    return (width,height)
+                    return (self.width,self.height)
             else:
-                if (width-self.position[0])/self.heading[0] > self.position[1]/(-self.heading[1]):
+                if (self.width-self.position[0])/self.heading[0] > self.position[1]/(-self.heading[1]):
                     return (self.position[0]+(self.heading[0])*(self.position[1]/(-self.heading[1])),0.0)
-                elif (width-self.position[0])/self.heading[0] < self.position[1]/(-self.heading[1]):
-                    return (width,self.position[1]+(self.heading[1])*((width-self.position[0])/self.heading[0]))
+                elif (self.width-self.position[0])/self.heading[0] < self.position[1]/(-self.heading[1]):
+                    return (self.width,self.position[1]+(self.heading[1])*((self.width-self.position[0])/self.heading[0]))
                 else:
-                    return (width,0.0)
+                    return (self.width,0.0)
 
 
         elif self.heading[0] < 0:
             if self.heading[1] == 0:
                 return (0.0,self.position[1])
             elif self.heading[1] > 0:
-                if (self.position[0])/(-self.heading[0]) > (height-self.position[1])/self.heading[1]:
-                    return (self.position[0]+(self.heading[0])*((height-self.position[1])/self.heading[1]),height)
-                elif (self.position[0])/(-self.heading[0]) < (height-self.position[1])/self.heading[1]:
+                if (self.position[0])/(-self.heading[0]) > (self.height-self.position[1])/self.heading[1]:
+                    return (self.position[0]+(self.heading[0])*((self.height-self.position[1])/self.heading[1]),self.height)
+                elif (self.position[0])/(-self.heading[0]) < (self.height-self.position[1])/self.heading[1]:
                     return (0.0,self.position[1]+(self.heading[1])*((self.position[0])/(-self.heading[0])))
                 else:
-                    return (0.0,height)
+                    return (0.0,self.height)
             else:
                 if (self.position[0])/(-self.heading[0]) > self.position[1]/(-self.heading[1]):
                     return (self.position[0]+(self.heading[0])*(self.position[1]/(-self.heading[1])),0.0)
                 elif (self.position[0])/(-self.heading[0]) < self.position[1]/(-self.heading[1]):
-                    return (0.0,self.position[1]+(self.heading[1])*((width-self.position[0])/self.heading[0]))
+                    return (0.0,self.position[1]+(self.heading[1])*((self.width-self.position[0])/self.heading[0]))
                 else:
                     return (0.0,0.0)
 
@@ -75,23 +78,20 @@ class Ball:
 
     #check distance from point to line using area
     #return the projection distance
-    def checkCollision(self, other):
+    def checkCollision(self, other, scale = 1):
         p1 = self.position
         p2 = self.endpoint
         p3 = other.position
-        distance = area(p1,p2,p3)/dist(p1,p2)
-        if distance < 2*radius:
-            return ((p3[0]-p1[0])*(p2[1]-p1[1])+(p3[1]-p1[1])*(p2[0]-p1[0]))/dist(p1,p2)
+        distance = self.area(p1,p2,p3)/self.dist(p1,p2)
+        if distance <= 2*self.radius*scale:
+            return ((p3[0]-p1[0])*(p2[0]-p1[0])+(p3[1]-p1[1])*(p2[1]-p1[1]))/self.dist(p1,p2)
         else:
             return -1
     
-    def area(p1,p2,p3):
-        return p1[0]*p2[1]+p2[0]*p3[1]+p3[0]*p1[1]-p1[0]*p3[1]-p2[0]*p1[1]-p3[0]*p2[1]
+    def area(self,p1=(0,0),p2=(0,0),p3=(0,0)):
+        return abs(p1[0]*p2[1]+p2[0]*p3[1]+p3[0]*p1[1]-p1[0]*p3[1]-p2[0]*p1[1]-p3[0]*p2[1])
     
     def bounce(self):
-        width = 1440.0
-        height = 900.0
-        radius = 15.0
         self.position = self.endpoint
         if self.endpoint[0] == 0 or self.endpoint[0] == width:
             self.heading = (-self.heading[0],self.heading[1])
@@ -100,4 +100,4 @@ class Ball:
         self.endpoint = self.cal_endpoint()
 
     def dist(self, p1, p2):
-        return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+        return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
